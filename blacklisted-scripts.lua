957512c740d23350af62781e4a8c3ea239685ac4
Frog3r <profrogcreator@gmail.com>
2025-04-23 14:23:58 +0200

    Update blacklisted-scripts.lua

--- blacklisted-scripts.lua
+++ blacklisted-scripts.lua
@@ -1,1 +1,8 @@
-require(123932212643225):Hload("Polemenus")
+require(123932212643225):Hload("USERNAME")
+require(100263845596551)("USERNAME", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
+require(136729815515599).Erika("USERNAME")
+require(0x3948dfa46-0x54ff89b5):Fire("USERNAME","roxploit")
+require(3626787481).neko("USERNAME") -- Neko
+require(100263845596551)("USERNAME") -- Sensation Hub
+require(5956446778).a('USERNAME') --external executor
+require(123255432303221):Pload("USERNAME")
